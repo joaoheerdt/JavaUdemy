6b5b326ade8411c643f9525e071710e3eba46b11
João Henrique <joaoheerdt777@gmail.com>
2026-05-30 15:44:32 -0300

    Resolvendo

---
+++ src/Vetores/application/Main.java
@@ -0,0 +1,32 @@
+package Vetores.application;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args0){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("How many numbers are you going to type?");
+        int n = sc.nextInt();
+
+        if ( n > 10) {
+            System.out.println("Error: The number must be between 1 and 10.");
+        }
+        else {
+            int[] vect = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                System.out.println("Enter the number:");
+                vect[i] = sc.nextInt();
+            }
+
+            System.out.println("Negative numbers:");
+            for (int i = 0; i < n; i++) {
+                if (vect[i] < 0)
+                {
+                    System.out.println(vect[i]);
+                }
+            }
+            sc.close();
+        }}}
