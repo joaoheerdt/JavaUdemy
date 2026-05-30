10c0d9224905ad71f3bf53537a2abdcab1b8cdde
João Henrique <joaoheerdt777@gmail.com>
2026-05-30 19:09:09 -0300

    Resolvendo

---
+++ src/Vetores/exercicio02/entities/People.java
@@ -0,0 +1,25 @@
+package Vetores.exercicio02.entities;
+
+public class People {
+    private String name;
+    private int age;
+    private double height;
+
+    public People(String name, int age, double height) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+}
