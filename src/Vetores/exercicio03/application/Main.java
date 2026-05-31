e8be3be9f7146c9b24fcea4d98acbd439437db67
João Henrique <joaoheerdt777@gmail.com>
2026-05-31 13:36:57 -0300

    Resolvendo vetor exercicio 03

---
+++ src/Vetores/exercicio03/application/Main.java
@@ -0,0 +1,32 @@
+package Vetores.exercicio03.application;
+
+import Vetores.exercicio03.entities.Guests;
+
+import java.util.Scanner;
+
+public class Main {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Guests[] vect = new Guests[10];
+
+        System.out.println("How many rooms will be rented?");
+        int numbersofRooms = sc.nextInt();
+
+        for (int i = 0; i < numbersofRooms; i++) {
+            System.out.println("Name: ");
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+        sc.close();
+    }
+}
---
+++ src/Vetores/exercicio03/entities/Guests.java
@@ -0,0 +1,20 @@
+package Vetores.exercicio03.entities;
+
+public class Guests {
+    private String guest;
+    private String email;
+
+    public Guests(String guest, String email) {
+        this.guest = guest;
+        this.email = email;
+    }
+
+    public String getGuest() {
+        return guest;
+    }
+
+    public String getEmail() {
+        return email;
+
+    }
+}
